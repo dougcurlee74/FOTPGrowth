0128eb07316551907364450480474441a49f664a
dougcurlee74 <61290459+dougcurlee74@users.noreply.github.com>
2020-02-26 12:43:12 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+var app = require('express')(); // Create an instance of an Express app
+
+var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+
+mobileApp.tables.add('TableName'); // Create a table entry for each table in the DB with default settings
+
+app.use(mobileApp);
+app.listen(process.env.PORT || 3000);
