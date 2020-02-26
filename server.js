52e999f6f5da3b36bac5f40fad33ac4fbb31ee31
dougcurlee74 <61290459+dougcurlee74@users.noreply.github.com>
2020-02-26 12:54:52 -0600

    added People table

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('TableName'); // Create a table entry for each table in the DB with default settings
+mobileApp.tables.add('People'); // Create a line like this for every table in the database
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
